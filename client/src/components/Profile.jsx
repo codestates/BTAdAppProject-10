5219e475cad4dad4c252d10a61bbd7673096fae8
Sam Rho <samrho.dev@gmail.com>
2022-10-27 15:10:45 +0900

    ui: button style 변경

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -4,28 +4,27 @@ import {Box, Button} from "@mui/material";
 import {fromWei} from 'web3-utils';
 
 export default function Profile() {
-    const {address, isConnected} = useAccount()
+    const {address, isConnected} = useAccount();
     const {data, isError, isLoading} = useBalance({
         addressOrName: address,
         formatUnits: 'ether'
-    })
+    });
     const {data: ensName} = useEnsName({address})
     const {connect} = useConnect({
         connector: new InjectedConnector(),
-    })
-    const {disconnect} = useDisconnect()
+    });
+    const {disconnect} = useDisconnect();
 
-    console.log(data);
     if (isConnected) return (
         <Box display="flex" flexDirection="column" alignItems="center">
             <Box>Connected to {ensName ?? address}</Box>
             <Box>
-                {(isLoading || isError) ? 'balance load error' : `${fromWei(data.value.toString(), 'ether')} ${data.symbol}`}
+                잔액: {(isLoading || isError) ? 'balance load error' : `${fromWei(data.value.toString(), 'ether')} ${data.symbol}`}
             </Box>
             <Box>
-                <Button onClick={disconnect}>disconnet</Button>
+                <Button variant="contained" color="error" onClick={disconnect}>disconnet</Button>
             </Box>
         </Box>
     )
-    return <Button onClick={() => connect()}>Connect Wallet</Button>
+    return <Button variant="outlined" onClick={() => connect()}>Connect Wallet</Button>
 }
